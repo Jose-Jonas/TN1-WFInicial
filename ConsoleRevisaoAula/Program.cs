130f91fa7032bfa39c14a1c5b22617d38072a5f3
jose.jaraujo1 <jose.jaraujo1@L130635504W10-1>
2025-02-26 22:02:46 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleRevisaoAula/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(" Digite a primeira nota");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota");
+double nota2 = double.Parse(Console.ReadLine());
+
+
+double media = (nota1 + nota2 ) / 2;
+Console.Clear();
+
+
+if (media >= 7)
+{
+    Console.WriteLine($"Aluno Aprovado : MÉDIA; {media}");
+}
+else if (media >= 5)
+{
+    Console.WriteLine($"Aluno Recuperação: MÉDIA : {media}");
+}
+else
+{
+    Console.WriteLine($"Aluno Reprovado: MÉDIA: {media}");
+}
