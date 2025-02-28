5d88cc671b2ba01babe525be857020fa3842e520
JOSE JONAS ARAUJO <jose.jaraujo1@senacsp.edu.br>
2025-02-27 22:25:54 -0300

    aa

---
+++ CEX3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CEX3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: "); int num1 = int.Parse(Console.ReadLine()!); Console.WriteLine("Digite o segundo número: "); int num2 = Convert.ToInt32(Console.ReadLine());//outra forma de converter  int maior = Math.Max(num1, num2); C
+            Console.WriteLine("Digite o primeiro número: ");
+
+            int num1 = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Digite o segundo número: ");
+
+            int num2 = Convert.ToInt32(Console.ReadLine());//outra forma de converter
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\nO Número maior é {maior} é maior"); //\n quebra de linha
+
+            ;
+
+        }
+    }
+}
