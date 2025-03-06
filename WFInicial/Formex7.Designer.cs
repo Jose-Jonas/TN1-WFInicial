81b9a9d57f4aca7ff0ea38e3b36eaa0221bc5492
JOSE JONAS ARAUJO <jose.jaraujo1@senacsp.edu.br>
2025-03-06 19:54:44 -0300

    Correção

--- WFInicial/Formex7.Designer.cs
+++ WFInicial/Formex7.Designer.cs
@@ -28,79 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            lbl1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
+            txtValorProduto = new TextBox();
+            txtNomeProduto = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
-            // label1
+            // lbl1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(62, 173);
-            label1.Name = "label1";
-            label1.Size = new Size(61, 15);
-            label1.TabIndex = 0;
-            label1.Text = "1°Produto";
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(19, 46);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(33, 15);
+            lbl1.TabIndex = 0;
+            lbl1.Text = "Valor";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(247, 173);
+            label2.Location = new Point(19, 109);
             label2.Name = "label2";
-            label2.Size = new Size(61, 15);
+            label2.Size = new Size(50, 15);
             label2.TabIndex = 1;
-            label2.Text = "2°Produto";
+            label2.Text = "Produto";
             // 
-            // label3
+            // txtValorProduto
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(62, 231);
-            label3.Name = "label3";
-            label3.Size = new Size(38, 15);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
+            txtValorProduto.Location = new Point(85, 101);
+            txtValorProduto.Name = "txtValorProduto";
+            txtValorProduto.Size = new Size(153, 23);
+            txtValorProduto.TabIndex = 2;
             // 
-            // label4
+            // txtNomeProduto
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(247, 231);
-            label4.Name = "label4";
-            label4.Size = new Size(38, 15);
-            label4.TabIndex = 3;
-            label4.Text = "label4";
+            txtNomeProduto.Location = new Point(85, 43);
+            txtNomeProduto.Name = "txtNomeProduto";
+            txtNomeProduto.Size = new Size(153, 23);
+            txtNomeProduto.TabIndex = 3;
             // 
-            // label5
+            // button1
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(62, 318);
-            label5.Name = "label5";
-            label5.Size = new Size(38, 15);
-            label5.TabIndex = 4;
-            label5.Text = "label5";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(247, 318);
-            label6.Name = "label6";
-            label6.Size = new Size(38, 15);
-            label6.TabIndex = 5;
-            label6.Text = "label6";
+            button1.Location = new Point(80, 158);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 23);
+            button1.TabIndex = 4;
+            button1.Text = "b";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Formex7
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(423, 478);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
+            ClientSize = new Size(291, 236);
+            Controls.Add(button1);
+            Controls.Add(txtNomeProduto);
+            Controls.Add(txtValorProduto);
             Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(lbl1);
             Name = "Formex7";
             Text = "Formex7";
             ResumeLayout(false);
@@ -109,11 +95,10 @@
 
         #endregion
 
-        private Label label1;
+        private Label lbl1;
         private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
+        private TextBox txtValorProduto;
+        private TextBox txtNomeProduto;
+        private Button button1;
     }
 }
--- WFInicial/Formex7.cs
+++ WFInicial/Formex7.cs
@@ -12,11 +12,39 @@ namespace WFInicial
 {
     public partial class Formex7 : Form
     {
+        public string[] ListaNomeProdutos = new string[6];
+        public double[] ListaValoresProdutos = new double[6];
+        public int contador = 0;
+        
         public Formex7()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador = contador + 1;
+            ListaNomeProdutos[contador - 1] = txtNomeProduto.Text;
+            ListaNomeProdutos[contador - 1] = txtValorProduto.Text;
+            Convert.ToDouble(txtValorProduto.Text);
 
+            txtNomeProduto.Text = "";
+            txtValorProduto.Text = "";
+
+            if (contador == 6)
+            {
+                double menor = ListaValoresProdutos.Min();
+                int pos_menor = Array.IndexOf(ListaValoresProdutos,menor);
+                string mensagem =
+                $"O Produto mais Barato é {ListaNomeProdutos[pos_menor]}" +
+                $"de Valor R$ {menor:F2}";
+
+                MessageBox.Show(mensagem);
+
+
+             
+            }
+
+        }
     }
 }
